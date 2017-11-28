92b9118a5fbbbdd596d5c5ea7177e78a1b519833
sibylleginger <rouxsibylle@gmail.com>
2017-11-28 15:54:37 +0100

    Update conf.php

--- eCommerce/config/conf.php
+++ eCommerce/config/conf.php
@@ -7,14 +7,14 @@ class Conf {
     'hostname' => 'webinfo',
     // At IUT, you have a database named after your login
     // On your computer, please create a database
-    'database' => 'legallon',
+    'database' => 'rouxs',
     // At IUT, it is your classical login
     // On your computer, you should have at least a 'root' account
-    'login' => 'legallon',
+    'login' => 'rouxs',
     // At IUT, it is your database password 
     // (=PHPMyAdmin pwd, INE by defaut)
     // On your computer, you created the pwd during setup
-    'password' => 'setopslegallo'
+    'password' => 'sibylle'
   );
    
   static public function getLogin() {

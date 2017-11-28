79ba5dc1e4c33321d4ce9597837f0038c25d1b1e
JulietteArz <arazojuliette@gmail.com>
2017-11-28 17:19:32 +0100

    Update list.php

--- eCommerce/view/peluche/list.php
+++ eCommerce/view/peluche/list.php
@@ -7,13 +7,13 @@ foreach ($tab_p as $p) {
 
     $pNom = htmlspecialchars($p->getNom());
 
-    $purlNom = rawurlencode($p->getNom());
+    $purlId = rawurlencode($p->getIdp());
     //Attention : Il ne faut pas encoder l’immatriculation déjà échappée pour le HTML. Il faut créer deux variables : 
     //une immatriculation pour le HTML et une pour les URLs.
     // rawurlencode sert à ne pas interpréter ce truc
 
     
-    echo '<p> <a href="index.php?action=read&nom='
-    . $purlNom . '" >' . $pNom . '</a> </p>';
+    echo '<p> <a href="index.php?action=read&idp='
+    . $purlId . '" >' . $pNom . '</a> </p>';
 }
 ?>
